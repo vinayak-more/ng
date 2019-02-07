5e6ecffaaa1fb90fabe684714052393312817d99
Vinayak More <vinayak.more0914@gmail.com>
2019-02-08 00:30:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, VERSION } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qrscanner';
+  ngVersion;
+
+    @ViewChild('scanner')
+    scanner: ZXingScannerComponent;
+
+    hasCameras = false;
+    hasPermission: boolean;
+    qrResultString: string;
+
+    availableDevices: MediaDeviceInfo[];
+    selectedDevice: MediaDeviceInfo;
+
+    ngOnInit(): void {
+      this.ngVersion = VERSION.full;
+        this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+            this.hasCameras = true;
+
+            console.log('Devices: ', devices);
+            this.availableDevices = devices;
+
+            // selects the devices's back camera by default
+            // for (const device of devices) {
+            //     if (/back|rear|environment/gi.test(device.label)) {
+            //         this.scanner.changeDevice(device);
+            //         this.selectedDevice = device;
+            //         break;
+            //     }
+            // }
+        });
+
+        this.scanner.camerasNotFound.subscribe((devices: MediaDeviceInfo[]) => {
+            console.error('An error has occurred when trying to enumerate your video-stream-enabled devices.');
+        });
+
+        this.scanner.permissionResponse.subscribe((answer: boolean) => {
+          this.hasPermission = answer;
+        });
+
+    }
+
+    handleQrCodeResult(resultString: string) {
+        console.log('Result: ', resultString);
+        this.qrResultString = resultString;
+    }
+
+    onDeviceSelectChange(selectedValue: string) {
+        console.log('Selection changed: ', selectedValue);
+        this.selectedDevice = this.scanner.getDeviceById(selectedValue);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZXingScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
